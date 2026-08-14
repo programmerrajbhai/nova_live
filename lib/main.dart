f981d951d4f8ec7c10a8f93a1e291a5bc2245b49
rajahmad <mdrajahmad686@gmail.com>
2026-08-14 16:27:13 +0600

    Rebrand Nova Live to NovaMate

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Nova Live',
+      title: 'Nova Mate',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
